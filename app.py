6e53d98b7956e1d3c1fd1add280b754084624340
mujupvt <mujupvt@gmail.com>
2026-09-03 16:05:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -407,7 +407,7 @@ if user_input:
     try:
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.6-flash",
             contents=user_input,
         )
 
